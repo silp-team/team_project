e580eb291f373db8643b4865d19e75e5cd77f567
DaleKim <97732494+muyaho-universe@users.noreply.github.com>
2022-12-02 15:08:16 +0900

    Create BoardServiceImpl.java

---
+++ src/main/java/com/example/parking/service/BoardServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.parking.service;
+
+import com.example.parking.bean.BoardVO;
+import com.example.parking.dao.BoardDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+
+    @Autowired
+    BoardDAO boardDAO;
+
+    @Override
+    public int insertBoard(BoardVO vo) {
+        return boardDAO.insertBoard(vo);
+    }
+
+    @Override
+    public int deletBoard(int seq) {
+        return boardDAO.deleteBoard(seq);
+    }
+
+    @Override
+    public int updateBoard(BoardVO vo) {
+        return boardDAO.updateBoard(vo);
+    }
+
+    @Override
+    public BoardVO getBoard(int seq) {
+        return boardDAO.getBoard(seq);
+    }
+
+    @Override
+    public List<BoardVO> getBoardList() {
+        return boardDAO.getBoardList();
+    }
+}
